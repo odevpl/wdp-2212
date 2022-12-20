f37640f9b9469766128bc5593c7d9c9183e405d6
Marek Skolik <marek250296@interia.pl>
2022-12-20 10:15:23 +0100

    added Brands to the Shop subpage

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -1,20 +1,24 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
+import Brands from '../../layout/Brands/Brands';
 
 const ProductList = () => {
   return (
-    <div className='container'>
-      <div className='row'>
-        <div className='col'>BANNER</div>
+    <div className={styles.root}>
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>BANNER</div>
+        </div>
+        <div className='row'>
+          <div className='col-9'>Product List</div>
+          <div className='col-3'>Filter</div>
+        </div>
+        <div className='row'>
+          <div className='col'>Brands</div>
+        </div>
       </div>
-      <div className='row'>
-        <div className='col-9'>Product List</div>
-        <div className='col-3'>Filter</div>
-      </div>
-      <div className='row'>
-        <div className='col'>Brands</div>
-      </div>
+      <Brands />
     </div>
   );
 };
