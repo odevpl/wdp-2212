b101ef6bfec5fdf3a597f1c941514b427c3c2d7a
Marek Skolik <marek250296@interia.pl>
2022-12-30 09:47:41 +0100

    added product list to the Shop subtitle

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -4,26 +4,29 @@ import styles from './ProductList.module.scss';
 import Brands from '../../layout/Brands/Brands';
 import FilterBySize from '../FilterBySize/FilterBySize';
 import FilterByCategories from '../FilterByCategories/FilterByCategories';
+import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
+import ChatBot from '../../features/ChatBot/ChatBot';
+import Banner from '../../features/Banner/Banner';
 
 const ProductList = () => {
   return (
     <div className={styles.root}>
       <div className='container'>
         <div className='row'>
-          <div className='col'>BANNER</div>
+          <div className='col'><Banner /></div>
         </div>
         <div className='row'>
-          <div className='col-9'>Product List</div>
-          <div className='col-3'>Filter
+          <div className='col-9'><NewFurniture amount={12} /></div>
+          <div className='col-3'>
           <FilterByCategories />
           <FilterBySize />
           </div>
         </div>
         <div className='row'>
-          <div className='col'>Brands</div>
+          <div className='col'><Brands /></div>
         </div>
       </div>
-      <Brands />
+      <ChatBot />
     </div>
   );
 };
